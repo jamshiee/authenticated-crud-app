36ce15096e733ebce9d7ea4ef685a88dad39dcac
jamshiee <jamsheedjaleel03@gmail.com>
2025-05-14 15:05:37 +0530

    Deployment Fixes

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 export const dbConnect = async () =>{
     try {
-        await mongoose.connect(process.env.MONGODB_URI)
+        await mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser: true, useUnifiedTopology: true })
         console.log("Db Connected")
     } catch (error) {
         console.log(error)
